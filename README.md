3ab3a630a96543d4e987de16484bc1700d9848f0
macias <bluedzins@wp.pl>
2021-11-22 20:20:59 +0100

    chapter 1

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 
 ## Magdalena Filak, Hiszpański w tłumaczeniach cz.1
 
-Pliki audio zawierają całe rozdziały, a nie poszczególne wypowiedzi. Jeżeli także wolisz bezpośredni dostęp do
+Pliki audio zawierają całe rozdziały, a nie poszczególne wypowiedzi. Jeżeli wolisz bezpośredni dostęp do
 każdej z kwestii, rozpakuj paczkę z lektorem hiszpańskim, zainstaluj ffmpega oraz basha, a następnie w katalogu,
 w którym znajdują się pliki mp3 uruchom skrypt hiszp_tlum-splitter.sh (to na razie tylko początek).
--- hiszp_tlum-splitter.sh
+++ hiszp_tlum-splitter.sh
@@ -2,9 +2,63 @@
 
 # https://www.shell-tips.com/bash/loops/
 
-CH=2
+function split_chapter
+{
+  local CH=$1
+  local -n CUT=$2
 
-CUT=(
+  printf -v CHSTR "%02d" $CH
+
+  for ((i=1 ; i<${#CUT[@]} ; ++i))
+  do
+    printf -v ISTR "%02d" $i
+    ffmpeg -ss ${CUT[$((i-1))]} -to ${CUT[$i]} -i 0$((CH+35))\ Unidad\ ${CH}\ Es.mp3 -c copy r${CHSTR}_w${ISTR}.mp3
+  done
+}
+
+TIMES=(
+00:00:04.7
+00:00:06.7
+00:00:09.2
+00:00:12.2
+00:00:15.2
+00:00:18.2
+00:00:20.5
+00:00:23
+00:00:27.1
+00:00:29.7
+00:00:31.8
+00:00:33.8
+00:00:36.1
+00:00:38.2
+00:00:40.6
+00:00:42.9
+00:00:45
+00:00:47.2
+00:00:49.7
+00:00:51.9
+00:00:54.7
+00:00:57.5
+00:00:59.7
+00:01:01.9
+00:01:04.3
+00:01:06.6
+00:01:09.2
+00:01:11.6
+00:01:16.1
+00:01:20.3
+00:01:23
+00:01:24.9
+00:01:27.1
+00:01:29.2
+00:01:31.7
+00:01:34.5
+00:01:38.3
+)
+
+split_chapter 1 TIMES
+
+TIMES=(
 00:00:03.8
 00:00:06
 00:00:08.3
@@ -44,11 +98,4 @@ CUT=(
 00:02:53.1
 )
 
-printf -v CHSTR "%02d" $CH
-
-for ((i=1 ; i<${#CUT[@]} ; ++i))
-do
-  printf -v ISTR "%02d" $i
-  ffmpeg -ss ${CUT[$((i-1))]} -to ${CUT[$i]} -i 037\ Unidad\ ${CH}\ Es.mp3 -c copy r${CHSTR}_w${ISTR}.mp3
-done
-
+split_chapter 2 TIMES
